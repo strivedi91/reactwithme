6cc4318bc619388c6c2101163b77bd0142370c54
Sachin Trivedi <sachindtrivedi@gmail.com>
2020-11-01 00:52:56 +0530

    initial checkin

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "React with me",
+    description: "A blog dedicated to javascript and react",
+  },
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,54 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`
+
+const HomePage = ({ data }) => {
+  return (
+    <div>
+      <div>
+        <h1>{data.site.siteMetadata.title}</h1>
+        <p>{data.site.siteMetadata.description}</p>
+      </div>
+
+      <div>
+        {data.allMdx.nodes.map(({ excerpt, frontmatter, fields }) => (
+          <div>
+            <Link to={fields.slug}>
+              <h1>{frontmatter.title}</h1>
+            </Link>
+            <p>{frontmatter.date}</p>
+            <p>{excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
